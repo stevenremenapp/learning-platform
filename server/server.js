1d4fac36a4cd53cdb417f030213f56dedc9a840b
stevenremenapp <remenapp@gmail.com>
2019-04-24 20:35:55 -0400

    Final tweaks to code vars for AWS deployment

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const db = knex({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
     }
 });
 
