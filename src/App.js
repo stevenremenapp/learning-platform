39e2d80cc8cc8cc1b85730b8c4db3be0a1a9ad15
stevenremenapp <remenapp@gmail.com>
2019-07-22 20:19:59 -0400

    Refactor shelf expand/closing state/animations to work for all shelves

--- src/App.js
+++ src/App.js
@@ -17,13 +17,17 @@ class App extends Component {
       isLoaded: false,
       courses: [],
       shelvesShowing: {
-        stashed: true
+        currentlyTaking: true,
+        wantToTake: true,
+        completed: true,
+        stashed: true        
       },
       shelfHeight: {
+        currentlyTaking: {},
+        wantToTake: {},
+        completed: {},
         stashed: {}
       }
-      //stashedCoursesShowing: true,
-      //stashedCoursesHeight: {}
     }
   }
 
@@ -66,24 +70,98 @@ class App extends Component {
   }
 
   componentDidUpdate = () => {
-      if (reportHeight) {
-        let stashedDiv = this.refs.shelfAccordionWrapper;
-        let stashedName = this.refs.shelfAccordionWrapper.getAttribute('name');
+      /*if (reportHeight) {
+        let stashedDiv = this.refs.stashedShelfAccordionWrapper;
+        let stashedName = this.refs.stashedShelfAccordionWrapper.getAttribute('name');
+        let completedDiv = this.refs.completedShelfAccordionWrapper;
+        let completedName = this.refs.completedShelfAccordionWrapper.getAttribute('name');
+
+        let stashedHeight = this.returnWrapperHeight(stashedDiv);
+        let completedHeight = this.returnWrapperHeight(completedDiv);*/
+
+        // switch (true) {
+        //   case this.state.shelvesShowing.completed:
+        //     this.setState({ shelfHeight: {[stashedName]: stashedHeight, [completedName]: completedHeight} });
+        //     break;
+          
+        // }
+
+        // LOOP THRU STATE AND SET HEIGHT TO 0 IF FALSE???
+
+        // for (let i = 0; i < this.state.shelvesShowing.length; i++) {
+          // if (this.state.shelvesShowing.completed) {
+          //   this.setState({ shelfHeight: {[stashedName]: stashedHeight, [completedName]: completedHeight} });
+          // } else if (this.state.shelvesShowing.stashed) {
+          //   this.setState({ shelfHeight: {[stashedName]: stashedHeight, [completedName]: completedHeight} });
+          // } else if (!this.state.shelvesShowing.completed) {
+          //   this.setState({ shelfHeight: { [stashedName]: stashedHeight, [completedName]: { height: 0 } } });
+          // } else {
+          //   this.setState({ shelfHeight: { [stashedName]: { height: 0 }, [completedName]: completedDiv } });
+          // }
+        // }
+        /*
+        this.state.shelvesShowing.completed ?
+        this.setState({ shelfHeight: {[stashedName]: stashedHeight, [completedName]: completedHeight} }) :
+        !this.state.shelvesShowing.completed ?
+        this.setState({ shelfHeight: { [stashedName]: stashedHeight, [completedName]: { height: 0 } } }) :
         this.state.shelvesShowing.stashed ?
-        this.setState({ shelfHeight: this.returnWrapperHeight(stashedDiv, stashedName) }) :
-        this.setState({ shelfHeight: { stashed: { height: 0 } } });
+        this.setState({ shelfHeight: {[stashedName]: stashedHeight, [completedName]: completedHeight} }) :
+        !this.state.shelvesShowing.stashed ?
+        this.setState({ shelfHeight: { [stashedName]: { height: 0 }, [completedName]: completedDiv } }) :
+        console.log("DONE");
       }
-      reportHeight = false;
+      reportHeight = false;*/
+
+      this.setShelfHeights();
     };
 
-  showOrHideStashedCourses = (e) => {
+  showOrHideShelves = (e) => {
     reportHeight = true;
     const currentShelvesShowing = {...this.state.shelvesShowing};
     currentShelvesShowing[e.currentTarget.getAttribute('name')] = !currentShelvesShowing[e.currentTarget.getAttribute('name')];
     this.setState({shelvesShowing: currentShelvesShowing});
   }
 
-  returnWrapperHeight = (node, name) => {
+  setShelfHeights = () => {
+    if (reportHeight) {
+      const stashedDiv = this.refs.stashedShelfAccordionWrapper;
+      const completedDiv = this.refs.completedShelfAccordionWrapper;
+      const wantToTakeDiv = this.refs.wantToTakeShelfAccordionWrapper;
+      const currentlyTakingDiv = this.refs.currentlyTakingShelfAccordionWrapper;
+
+      const stashedName = stashedDiv.getAttribute('name');
+      const completedName = completedDiv.getAttribute('name');
+      const wantToTakeName = wantToTakeDiv.getAttribute('name');
+      const currentlyTakingName = currentlyTakingDiv.getAttribute('name');
+
+      const stashedHeight = this.state.shelvesShowing.stashed ? this.returnWrapperHeight(stashedDiv) : { height: 0 };
+      const completedHeight = this.state.shelvesShowing.completed ? this.returnWrapperHeight(completedDiv) : { height: 0 };
+      const wantToTakeHeight = this.state.shelvesShowing.wantToTake ? this.returnWrapperHeight(wantToTakeDiv) : { height: 0 };
+      const currentlyTakingHeight = this.state.shelvesShowing.currentlyTaking ? this.returnWrapperHeight(currentlyTakingDiv) : { height: 0 };
+
+      this.setState({ shelfHeight:
+        { [stashedName]: stashedHeight,
+          [completedName]: completedHeight,
+          [wantToTakeName]: wantToTakeHeight,
+          [currentlyTakingName]: currentlyTakingHeight
+        }
+      });
+
+      // this.state.shelvesShowing.completed ?
+      // this.setState({ shelfHeight: {[stashedName]: stashedHeight, [completedName]: completedHeight} }) :
+      // !this.state.shelvesShowing.completed ?
+      // this.setState({ shelfHeight: { [stashedName]: stashedHeight, [completedName]: { height: 0 } } }) :
+      // this.state.shelvesShowing.stashed ?
+      // this.setState({ shelfHeight: {[stashedName]: stashedHeight, [completedName]: completedHeight} }) :
+      // !this.state.shelvesShowing.stashed ?
+      // this.setState({ shelfHeight: { [stashedName]: { height: 0 }, [completedName]: completedDiv } }) :
+      console.log("DONE");
+    }
+
+    reportHeight = false;
+  }
+
+  returnWrapperHeight = (node) => {
 
     const containerStyle = {
       display: "inline-block",
@@ -108,11 +186,26 @@ class App extends Component {
 
     reportHeight = true;
 
-    //return { height };
-    return {
-        [name]: { height }
-    }
+    // return {
+    //     [name]: { height }
+    // }
+    return { height: height };
   }
+
+  // returnAllShelfHeightsObject = () => {
+  //   let stashedDiv = this.refs.stashedShelfAccordionWrapper;
+  //   let stashedName = this.refs.stashedShelfAccordionWrapper.getAttribute('name');
+  //   let completedDiv = this.refs.completedShelfAccordionWrapper;
+  //   let completedName = this.refs.completedShelfAccordionWrapper.getAttribute('name');
+  //   let stashed =  this.returnWrapperHeight(stashedDiv, stashedName);
+  //   let completed = this.returnWrapperHeight(completedDiv, completedName);
+  //   let allShelfHeightsObject = {
+  //     stashed,
+  //     completed
+  //   }
+  //   console.log(allShelfHeightsObject);
+  //   return allShelfHeightsObject;
+  // }
 
   returnEmptyShelfHtml = () => {
     return (
@@ -134,77 +227,122 @@ class App extends Component {
         return (
           <div className="App">
             <div className="shelf">
-              <h2>Currently Taking:</h2>
-              {
-                courses.filter(course => course.shelf === "currentlyTaking").length > 0 ?
-                <div>
-                {courses.filter(course => course.shelf === "currentlyTaking").map((course) => {
-                  return (
-                    <Course
-                      key={course.id}
-                      {...course}
-                      updateCourseLocation={this.updateCourseLocation}
-                    />
-                  )
-                })}
-                </div> :
-                <div>
-                  {this.returnEmptyShelfHtml()}
-                </div>
-              }
-            </div>
-            <div className="shelf">
-              <h2>Want to Take:</h2>
-              {
-                courses.filter(course => course.shelf === "wantToTake").length > 0 ?
-                <div>
-                {courses.filter(course => course.shelf === "wantToTake").map((course) => {
-                  return (
-                    <Course
-                      key={course.id}
-                      {...course}
-                      updateCourseLocation={this.updateCourseLocation}
-                    />
-                  )
-                })}
-                </div> :
-                <div>
-                  {this.returnEmptyShelfHtml()}
-                </div>
-              }
-            </div>
-            <div className="shelf">
-              <h2>Completed:</h2>
-              {
-                courses.filter(course => course.shelf === "completed").length > 0 ?
-                <div>
-                {courses.filter(course => course.shelf === "completed").map((course) => {
-                  return (
-                    <Course
-                      key={course.id}
-                      {...course}
-                      updateCourseLocation={this.updateCourseLocation}
-                    />
-                  )
-                })}
-                </div> :
-                <div>
-                  {this.returnEmptyShelfHtml()}
-                </div>
-              }
-            </div>
-            <div className="shelf">
-              <div className="stashedContainer" name="stashed" onClick={this.showOrHideStashedCourses}>
-                <div className="stashedTitleContainer">
-                  <h2 className="stashedTitle">Stashed:</h2>
-                  <div className="stashedShowHideContainer">
-                    <span className={`stashedShowHideText ${this.state.shelvesShowing.stashed ? "hide" : ""}`} id="stashedShowHideText">{this.state.shelvesShowing.stashed ? "HIDE" : "SHOW"}</span>
-                    <img src={require('./icons/add-black.svg')} alt="Icon to open or close stashed courses." className={`stashedIcon ${this.state.shelvesShowing.stashed ? "hide" : ""}`} />
+              <div className="shelfContainer" name="currentlyTaking" onClick={this.showOrHideShelves}>
+                <div className="shelfTitleContainer">
+                  <h2 className="shelfTitle">Currently Taking:</h2>
+                  <div className="shelfShowHideContainer">
+                    <span className={`shelfShowHideText ${this.state.shelvesShowing.currentlyTaking ? "hide" : ""}`}>{this.state.shelvesShowing.currentlyTaking ? "HIDE" : "SHOW"}</span>
+                    <img src={require('./icons/add-black.svg')} alt="Icon to open or close stashed courses." className={`shelfIcon ${this.state.shelvesShowing.currentlyTaking ? "hide" : ""}`} />
                   </div>
                 </div>
               </div>
               <div
-                ref="shelfAccordionWrapper"
+                ref="currentlyTakingShelfAccordionWrapper"
+                className="shelfAccordionWrapper"
+                name="currentlyTaking"
+                style={this.state.shelfHeight.currentlyTaking}
+                >
+                {
+                  courses.filter(course => course.shelf === "currentlyTaking").length > 0 ?
+                  <div>
+                      {courses.filter(course => course.shelf === "currentlyTaking").map((course) => {
+                        return (
+                          <Course
+                            key={course.id}
+                            {...course}
+                            updateCourseLocation={this.updateCourseLocation}
+                          />
+                        )
+                      })}
+                  </div> :
+                  <div>
+                    {this.returnEmptyShelfHtml()}
+                  </div>
+                }
+              </div>
+            </div>
+            <div className="shelf">
+              <div className="shelfContainer" name="wantToTake" onClick={this.showOrHideShelves}>
+                <div className="shelfTitleContainer">
+                  <h2 className="shelfTitle">Want To Take:</h2>
+                  <div className="shelfShowHideContainer">
+                    <span className={`shelfShowHideText ${this.state.shelvesShowing.wantToTake ? "hide" : ""}`}>{this.state.shelvesShowing.wantToTake ? "HIDE" : "SHOW"}</span>
+                    <img src={require('./icons/add-black.svg')} alt="Icon to open or close stashed courses." className={`shelfIcon ${this.state.shelvesShowing.wantToTake ? "hide" : ""}`} />
+                  </div>
+                </div>
+              </div>
+              <div
+                ref="wantToTakeShelfAccordionWrapper"
+                className="shelfAccordionWrapper"
+                name="wantToTake"
+                style={this.state.shelfHeight.wantToTake}
+                >
+                {
+                  courses.filter(course => course.shelf === "wantToTake").length > 0 ?
+                  <div>
+                      {courses.filter(course => course.shelf === "wantToTake").map((course) => {
+                        return (
+                          <Course
+                            key={course.id}
+                            {...course}
+                            updateCourseLocation={this.updateCourseLocation}
+                          />
+                        )
+                      })}
+                  </div> :
+                  <div>
+                    {this.returnEmptyShelfHtml()}
+                  </div>
+                }
+              </div>
+            </div>
+            <div className="shelf">
+              <div className="shelfContainer" name="completed" onClick={this.showOrHideShelves}>
+                <div className="shelfTitleContainer">
+                  <h2 className="shelfTitle">Completed:</h2>
+                  <div className="shelfShowHideContainer">
+                    <span className={`shelfShowHideText ${this.state.shelvesShowing.completed ? "hide" : ""}`}>{this.state.shelvesShowing.completed ? "HIDE" : "SHOW"}</span>
+                    <img src={require('./icons/add-black.svg')} alt="Icon to open or close stashed courses." className={`shelfIcon ${this.state.shelvesShowing.completed ? "hide" : ""}`} />
+                  </div>
+                </div>
+              </div>
+              <div
+                ref="completedShelfAccordionWrapper"
+                className="shelfAccordionWrapper"
+                name="completed"
+                style={this.state.shelfHeight.completed}
+                >
+                {
+                  courses.filter(course => course.shelf === "completed").length > 0 ?
+                  <div>
+                      {courses.filter(course => course.shelf === "completed").map((course) => {
+                        return (
+                          <Course
+                            key={course.id}
+                            {...course}
+                            updateCourseLocation={this.updateCourseLocation}
+                          />
+                        )
+                      })}
+                  </div> :
+                  <div>
+                    {this.returnEmptyShelfHtml()}
+                  </div>
+                }
+              </div>
+            </div>
+            <div className="shelf">
+              <div className="shelfContainer" name="stashed" onClick={this.showOrHideShelves}>
+                <div className="shelfTitleContainer">
+                  <h2 className="shelfTitle">Stashed:</h2>
+                  <div className="shelfShowHideContainer">
+                    <span className={`shelfShowHideText ${this.state.shelvesShowing.stashed ? "hide" : ""}`}>{this.state.shelvesShowing.stashed ? "HIDE" : "SHOW"}</span>
+                    <img src={require('./icons/add-black.svg')} alt="Icon to open or close stashed courses." className={`shelfIcon ${this.state.shelvesShowing.stashed ? "hide" : ""}`} />
+                  </div>
+                </div>
+              </div>
+              <div
+                ref="stashedShelfAccordionWrapper"
                 className="shelfAccordionWrapper"
                 name="stashed"
                 style={this.state.shelfHeight.stashed}
